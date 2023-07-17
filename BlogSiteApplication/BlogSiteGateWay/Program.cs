51c86506dfb444cbe754234ce226ffcf1ca5858c
LakshmiNarayana <PerlaLakshmi.Narayana@cognizant.com>
2023-07-17 13:42:37 +0530

    API Gateway implementation started

---
+++ BlogSiteApplication/BlogSiteGateWay/Program.cs
@@ -0,0 +1,33 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+var builder = WebApplication.CreateBuilder(args);
+
+IConfiguration configuration = new ConfigurationBuilder().AddJsonFile("Ocelot.json").Build();
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddOcelot(configuration);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseOcelot();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors(o => o.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
